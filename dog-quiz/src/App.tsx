780accf1962663ba5ef6511605db79ea094e591e
keinkondo <keinkondo@dnode.co.jp>
2026-01-22 14:19:28 +0900

    [ add ] top追加

--- dog-quiz/src/App.tsx
+++ dog-quiz/src/App.tsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import dogsImage from "./assets/animal_quiz.png";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App: React.FC = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <h1 className="title">犬種クイズ</h1>
+      <img src={dogsImage} alt="犬クイズ" className="image" />
+      <div className="button-container">
+        <button className="start-button">スタート</button>
+        <button className="rank-button">マイランク</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
