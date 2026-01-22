5d06812a41fb68e0acd40cde04850bcb007b351e
keinkondo <keinkondo@dnode.co.jp>
2026-01-22 15:46:55 +0900

    [ fix ] ポイントごとにバッジの表示を可変できるように修正

--- dog-quiz/src/features/myRank/pages/MyRank.tsx
+++ dog-quiz/src/features/myRank/pages/MyRank.tsx
@@ -1,6 +1,9 @@
 import { useNavigate } from "react-router-dom";
 import "./MyRank.css";
 import { useState } from "react";
+import bronze from "../../../assets/bronze.png";
+import silver from "../../../assets/silver.png";
+import gold from "../../../assets/gold.png";
 /**
  * マイランクコンポーネント
  */
@@ -11,6 +14,20 @@ const MyRank: React.FC = () => {
     const storedPoints = localStorage.getItem("point");
     return storedPoints ? parseInt(storedPoints, 10) : 0;
   });
+
+  // バッジの画像をポイントに応じて選択
+  const getBadgeImage = (level: "初級" | "中級" | "上級") => {
+    if (level === "初級" && points >= 10) {
+      return bronze;
+    }
+    if (level === "中級" && points >= 50) {
+      return silver;
+    }
+    if (level === "上級" && points >= 100) {
+      return gold;
+    }
+    return undefined; // デフォルトは no badge
+  };
 
   const handleShare = () => {
     const text = encodeURIComponent(
@@ -28,16 +45,46 @@ const MyRank: React.FC = () => {
       <h2 className="points-title">ポイント数：{points}</h2>
       <h2 className="points-title mt-30">所有バッジ：</h2>
       <div className="badge-row">
+        {/* 初級 */}
         <div className="badge-container">
-          <div className="badge-circle">no badge</div>
+          {getBadgeImage("初級") ? (
+            <img
+              src={getBadgeImage("初級")}
+              alt="初級バッジ"
+              className="badge-image"
+            />
+          ) : (
+            <div className="badge-circle">no badge</div>
+          )}
+
           <p className="badge-label">初級</p>
         </div>
+        {/* 中級 */}
         <div className="badge-container">
-          <div className="badge-circle">no badge</div>
+          {getBadgeImage("中級") ? (
+            <img
+              src={getBadgeImage("中級")}
+              alt="中級バッジ"
+              className="badge-image"
+            />
+          ) : (
+            <div className="badge-circle">no badge</div>
+          )}
+
           <p className="badge-label">中級</p>
         </div>
+        {/* 上級 */}
         <div className="badge-container">
-          <div className="badge-circle">no badge</div>
+          {getBadgeImage("上級") ? (
+            <img
+              src={getBadgeImage("上級")}
+              alt="上級バッジ"
+              className="badge-image"
+            />
+          ) : (
+            <div className="badge-circle">no badge</div>
+          )}
+
           <p className="badge-label">上級</p>
         </div>
       </div>
